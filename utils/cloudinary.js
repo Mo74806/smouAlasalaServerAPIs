3d71e71c4acee2daab81df6b1943447b4430138f
Mo74806 <m.khaled201774@gmail.com>
2022-11-24 17:24:47 +0200

    add

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,8 +2,8 @@ const cloudinary = require('cloudinary').v2;
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 cloudinary.config({
-  cloud_name: 'dkkjpuvma',
-  api_key: '973479728989825',
-  api_secret: 'Lk142QT16EhmTgHM-o7pEKrc2GQ'
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET
 });
 module.exports = { cloudinary };
